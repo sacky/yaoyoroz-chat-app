f85b9544d5d25d1271dc7bb544b6f409edb49ded
Sackey <sackey@Sakai-no-MacBook-Pro.local>
2018-05-31 01:02:34 +0900

    Adjust styles

--- chat.js
+++ chat.js
@@ -1,4 +1,5 @@
-/* センサ値 -> 佐藤さんのAIで処理 -> 自然言語を生成？ */
+/* センサ値 -> MTのAIで処理 -> 自然言語を生成？ */
+
 // ルールベース
 // 湿度XX%以下、気温YY℃以上
 const message0 = "ぼく、かびそうだよ...";
@@ -24,16 +25,12 @@ const messages = [message0, message1, message2, message3];
         let img_src;
         let flag = false;
         if(msg == messages[0]){
-            // https://publicdomainq.net/tote-bag-0013084/
             img_src =　"/img/bag.jpg";
         } else if(msg == messages[1]){
-            // http://kids.wanpug.com/illust100.html
             img_src = "/img/kasa.png";
         } else if(msg == messages[2]){
-            // http://01.gatag.net/tag/%E8%A1%A3%E6%9C%8D%EF%BC%88%E8%A1%A3%E9%A1%9E%EF%BC%89/
             img_src = "/img/huku1.png";
         } else if(msg == messages[3]){
-            // http://sozaizchi.com/sozai/illust/kodomofuku/03.html
             img_src = "/img/huku2.png";
         } else {
             img_src = "/img/me.png";
@@ -46,19 +43,12 @@ const messages = [message0, message1, message2, message3];
             $('#messages').append($('<span>').html('<div class="balloon6"><div class="faceicon"><img src="'+ img_src +'" /></div><div class="chatting"><div class="says"><p>' + msg + '</p></div></div></div>'));
         }
 
-
-
         window.scrollTo(0, document.body.scrollHeight);
     });
-
 
     $('form').submit(function(){
       socket.emit('chat message', $('#m').val());
       $('#m').val('');
       return false;
     });
-    // socket.on('chat message', function(msg){
-    //   $('#messages').append($('<li>').text(msg));
-    //   window.scrollTo(0, document.body.scrollHeight);
-    // });
   });
